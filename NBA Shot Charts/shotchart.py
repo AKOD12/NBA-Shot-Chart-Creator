88eb23c465c2ff9dd5ed037e10d55fcf741c268f
Ankith Kodali <ankith.kodali@gmail.com>
2021-06-29 16:56:49 -0400

    Update shotchart.py

--- NBA Shot Charts/shotchart.py
+++ NBA Shot Charts/shotchart.py
@@ -11,7 +11,7 @@ import urllib.request
 
 response = shotchartdetail.ShotChartDetail(
 	team_id=0,
-	player_id=1629622,
+	player_id=3032977,
 	season_nullable='2020-21',
 	season_type_all_star='Regular Season'
 )
@@ -25,7 +25,7 @@ df = pd.DataFrame(rows)
 df.columns = headers
 
 # write to csv file
-#df.to_csv("bryn_forbes.csv", index=False)
+#df.to_csv("giannis.csv", index=False)
 
 sns.set_style("white")
 sns.set_color_codes()
@@ -35,56 +35,48 @@ sns.set_color_codes()
 
 
 def draw_court(ax=None, color='black', lw=2, outer_lines=False):
-    # If an axes object isn't provided to plot onto, just get current one
     if ax is None:
         ax = plt.gca()
 
-    # Create the various parts of an NBA basketball court
-
-    # Create the basketball hoop
-    # Diameter of a hoop is 18" so it has a radius of 9", which is a value
-    # 7.5 in our coordinate system
+    #create hoop
     hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
 
-    # Create backboard
+    #create backboard
     backboard = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
 
-    # The paint
-    # Create the outer box 0f the paint, width=16ft, height=19ft
+    #create the outer box of the paint, width=16ft, height=19ft
     outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color,
                           fill=False)
-    # Create the inner box of the paint, widt=12ft, height=19ft
+    #create the inner box of the paint, widt=12ft, height=19ft
     inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color,
                           fill=False)
 
-    # Create free throw top arc
+    #create free throw top arc
     top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180,
                          linewidth=lw, color=color, fill=False)
-    # Create free throw bottom arc
+    #create free throw bottom arc
     bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0,
                             linewidth=lw, color=color, linestyle='dashed')
-    # Restricted Zone, it is an arc with 4ft radius from center of the hoop
+    #restricted Zone, it is an arc with 4ft radius from center of the hoop
     restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw,
                      color=color)
 
-    # Three point line
-    # Create the side 3pt lines, they are 14ft long before they begin to arc
+    #create the side 3pt lines, they are 14ft long before they begin to arc
     corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
                                color=color)
     corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
-    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
-    # I just played around with the theta values until they lined up with the 
-    # threes
+    #3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    #I just played around with the theta values until they lined up 
     three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
                     color=color)
 
-    # Center Court
+    #half court
     center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0,
                            linewidth=lw, color=color)
     center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
                            linewidth=lw, color=color)
 
-    # List of the court elements to be plotted onto the axes
+    #list of the court elements to be plotted onto the axes
     court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
                       bottom_free_throw, restricted, corner_three_a,
                       corner_three_b, three_arc, center_outer_arc,
@@ -115,7 +107,7 @@ draw_court(outer_lines=True)
 plt.xlim(300,-300)
 plt.ylim(-50,500)
 
-plt.title("Max Strus Made Baskets Shot Chart")
+plt.title("Giannis Antetokounmpo Made Baskets Shot Chart")
 plt.text(0, 300, "Data: nba.stats.com\nGraph made by Ankith Kodali")
 
 pic = urllib.request.urlretrieve("http://stats.nba.com/media/players/230x185/1627854.png",
